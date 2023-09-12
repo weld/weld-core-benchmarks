c34c5489ab75acececd239ff33332007d9590e1e
Matej Novotny <manovotn@redhat.com>
2023-09-12 16:42:21 +0200

    Add workload simulation to the bean invocation via JMH Blackhole API.

--- src/main/java/org/jboss/weld/benchmark/core/invokableMethods/InvokableBean.java
+++ src/main/java/org/jboss/weld/benchmark/core/invokableMethods/InvokableBean.java
@@ -1,13 +1,17 @@
 package org.jboss.weld.benchmark.core.invokableMethods;
 
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.invoke.Invokable;
+import org.openjdk.jmh.infra.Blackhole;
 
-@RequestScoped
+@Dependent
 public class InvokableBean {
 
     @Invokable
     public String ping(String s, Boolean b) {
+        // simulate some actual form of work being done in the bean
+        // the number is abstract but simulates the same amount of work for every invocation
+        Blackhole.consumeCPU(200);
         return InvokableBean.class.getSimpleName();
     }
 }
---
+++ src/main/java/org/jboss/weld/benchmark/core/invokableMethods/InvokableMethodManualLookupBeanInvocation.java
@@ -0,0 +1,25 @@
+package org.jboss.weld.benchmark.core.invokableMethods;
+
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.spi.CDI;
+
+public class InvokableMethodManualLookupBeanInvocation extends InvokableMethodBenchmarkBase {
+    @Override
+    protected String performInvoke() {
+        // WIP - naive assumption is that this should be just about the same performance as InvokableMethodBenchmarkLookup
+        Instance<Object> lookup = CDI.current().getBeanContainer().createInstance();
+        InvokableBean bean = null;
+        String string = null;
+        Boolean bool = null;
+        try {
+            bean = lookup.select(InvokableBean.class).get();
+            string = lookup.select(String.class).get();
+            bool = lookup.select(Boolean.class).get();
+            return invoker.invoke(bean, new Object[]{string, bool});
+        } finally {
+            lookup.destroy(bean);
+            lookup.destroy(string);
+            lookup.destroy(bool);
+        }
+    }
+}
